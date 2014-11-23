bb1fb67291f3fcab927829c117a9375859410552
rocky_manobi <tak.koroki@gmail.com>
2014-11-24 00:01:46 +0900

    experiment

--- web.js
+++ web.js
@@ -58,8 +58,10 @@ io.on('connection', function(socket){
   socket.on("post image", function(data){
     console.log( "=====================" );
     console.log( data.file );
-    io.emit("rcv image", { name: data.name, maxSize: data.maxSize } );
     console.log( "=====================" );
+    setTimeout(function(){
+      io.emit("rcv image", { file: data.file, name: data.name, maxSize: data.maxSize } );
+    },1000 );
   });
 
 });
